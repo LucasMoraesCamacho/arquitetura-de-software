b1d196152efd11e9438f311f300854a442b7953c
LucasMoraesCamacho <92307717+LucasMoraesCamacho@users.noreply.github.com>
2022-03-28 21:29:12 -0300

    Add files via upload

---
+++ Calculadora/src/Operacoes.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucas
+ */
+
+public class Operacoes {
+    float result;
+    public float SumN (float N1, float N2) {
+        result = N1+N2;
+        return result;
+    }
+    
+    public float SubN (float N1, float N2) {
+        result = N1-N2;
+        return result;
+    }
+    
+    public float MulN (float N1, float N2) {
+        result = N1*N2;
+        return result;
+    }
+    
+    public float DivN (float N1, float N2) {
+       
+        result = N1/N2;
+        return result;
+    }
+}
